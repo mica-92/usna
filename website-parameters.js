62134cf2497c0d53043d7d123532575b6aab36f0
Mica <68188565+mica-92@users.noreply.github.com>
2025-09-03 09:43:48 -0300

    First Website

---
+++ website-parameters.js
@@ -0,0 +1,11 @@
+// website-parameters.js
+const websiteParameters = {
+    ubicacion: "Edificio Panamericana 50, Pilar, Provincia de Buenos Aires, Argentina",
+    telefono: "(011) 4245-7890",
+    email: "info@usna.com.ar",
+    horarios: {
+        semana: "Lunes a Viernes: 8:00 - 21:00",
+        sabados: "Sábados: 9:00 - 18:00",
+        domingos: "Domingos: Cerrado"
+    },
+};
